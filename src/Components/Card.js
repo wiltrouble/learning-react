bc5f22b41563681fc76ded383bd92df83863ed13
Wilson Lopez <wilson.lopez@jalasoft.com>
2021-10-17 00:06:37 -0400

    refactor card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,28 +1,29 @@
 import React from 'react'
 import { MdOutlineDone, MdModeEditOutline } from "react-icons/md";
 import { AiFillHeart } from "react-icons/ai";
-import { AiFillEdit, AiFillDelete } from "react-icons/ai";
-import { FiEdit2 } from "react-icons/fi";
+import { AiFillDelete } from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
-const Card = ({task}) => {
+
+const Card = ({task, deleteTask, togleTaskDone}) => {
   return (
     <div className="bg-gray-900 rounded-lg flex flex-row mb-4 mx-2">
       <div className="flex flex-col items-center justify-center space-around m-2 bg-red-900">
         <div className="bg-red-100 rounded-full h-20 w-20 flex items-center justify-center">
           <AiFillHeart size="40" color="black"/>
         </div>
-        <button
-          className="bg-red-600 hover:bg-red-500 py-2 px-4 mr-2">Done
-        </button>
       </div>
       <div className="flex flex-col py-6 w-4/5">
         <h2>{task.title}</h2>
         <p>{task.description}</p>        
       </div>
       <div className="flex flex-col items-center justify-center space-between">
-        <MdModeEditOutline className="m-2" size={24}/>
-        <AiFillDelete className="m-2" size={24}/>
-        <MdOutlineDone className="m-2" size={24}/>
+        <Link 
+          to={`/edit/${task.id}`}>
+          <MdModeEditOutline className="m-2" size={24}/>
+        </Link>
+        <AiFillDelete className="m-2" size={24} onClick={() => deleteTask(task.id)}/>
+        <MdOutlineDone className="m-2" size={24} onClick={() => togleTaskDone(task.id)}/>
       </div>
     </div>
   )
--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -11,31 +11,9 @@ const TaskList = () => {
     <div className="flex justify-center">
       {/* <button onClick={() => deleteTask()}>Delete all</button> */}
       <div className="grid grid-cols-4">
-        {tasks && tasks.map(task => (
-          <div className="bg-gray-900 px-20 py-5 text-white, shadow-2x1 mb-4 flex justify-between mx-2"
-            key={task.id}>
-            <div>
-              <h1>{task.title}</h1>
-              <p>{task.description}</p>
-              <button
-                onClick={() => togleTaskDone(task.id)} 
-                className="bg-purple-600 hover:bg-purple-500 py-1 px-3 mt-2">
-                {task.done ? 'undone':'done'}
-              </button>
-            </div>
-            <div>
-              <Link 
-                to={`/edit/${task.id}`}
-                className="bg-gray-600 hover:bg-gray-500 py-2 px-4 mr-2">Edit</Link>
-              <button
-                onClick={() => deleteTask(task.id)} 
-              className="bg-red-600 hover:bg-red-500 py-2 px-4 mr-2">Delete</button>
-            </div>
-          </div>
-        ))}
         {
           tasks.map(task => (
-            <Card task={task}/>
+            <Card task={task} deleteTask={deleteTask} togleTaskDone={togleTaskDone}/>
           ))
         }
         {/* <Card/> */}
